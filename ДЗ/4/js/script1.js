e61d8c19cff2dabc3d00ef7d9c394b22c8fdfd3f
Veniamin <Yashunichkin@gmail.com>
2019-09-24 11:57:34 +0300

    correct background size

---
+++ ДЗ/4/js/script1.js
@@ -0,0 +1,13 @@
+/*var WhiteButton = document.querySelector('WhiteButton'),
+    RedButton = document.querySelector('RedButton');
+
+    function ColorRed() {
+        body.style.backgroundColor = 'Red';
+        return false;
+    }
+	function ColorWhite() {
+        body.style.backgroundColor = 'White';
+        return false;
+    }
+    RedButton.addEventListener('click', ColorRed, false);
+    WhiteButton.addEventListener('click', ColorWhite, false);
